532cf4b648091de1ce245b05e4927d134761cf19
mitchell <mitchellwortsman@gmail.com>
2022-12-18 21:49:58 +0000

    examples scripts and plots

---
+++ scripts/examples/loss_plot.py
@@ -0,0 +1,126 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def proc(df, lim):
+    df.drop(df[df[0] == 0].index[1:], axis=0, inplace=True)
+    df.drop_duplicates(0, keep='last', inplace=True)
+    if lim > 0:
+        df = df[df[0] < lim]
+    return df
+if __name__ == '__main__':
+    kernel_size = 40
+    min_loss = 14
+    max_scaler = 1
+    log_level = 3
+
+
+    file_list = []
+    file_list = [
+        ('b32-beta2-05', 'lr=2e-3, beta2=0.5, bs=8k', 'C0', -1),
+        ('b32-beta2-098', 'lr=2e-3, beta2=0.98, bs=8k', 'C1', -1),
+        ('b32-beta2-0999', 'lr=2e-3, beta2=0.999, bs=8k', 'C2', -1),#3900),
+    ]
+    #file_list = file_list[1:2]
+    #file_list = file_list[:2]
+
+    fig, axlist = plt.subplots(log_level, 1, figsize=(8, 5 * log_level))
+    if log_level == 1:
+        axlist = [axlist]
+    for j, (file, name, color, lim) in enumerate(file_list):
+
+        if log_level >= 1:
+            ax = axlist[0]
+            for i in range(1):
+                df = pd.read_csv(f'/fsx-labs/mitchellw/experiments/blowup-examples/{file}/data/{i}/loss.csv', names=list(range(2)))
+                df = proc(df, lim)
+                ax.plot(df.iloc[:, 0], np.minimum(min_loss, df.iloc[:, 1]), color=color, alpha=0.5)#, label=name)# alpha=0.5,
+                
+                kernel = np.ones(kernel_size) / kernel_size
+                data_convolved = np.convolve(df.iloc[:, 1], kernel, mode='same')
+                data_convolved = data_convolved[kernel_size:-kernel_size]
+                ax.plot(df.iloc[:, 0][kernel_size:-kernel_size], np.minimum(min_loss, data_convolved), color=color, label=name)
+                ax.set_ylabel('Loss')
+                ax.set_yscale('log')
+                #ax.set_xscale('log')
+
+        
+        if log_level >= 2:
+            ax = axlist[1]
+            for i in range(1):
+                df = pd.read_csv(f'/fsx-labs/mitchellw/experiments/blowup-examples/{file}/data/{i}/features-module.visual.transformer.resblocks.10.csv', names=list(range(4)))
+                df = proc(df, lim)
+                #df = pd.read_csv(f'/fsx-labs/mitchellw/experiments/blowup-examples/{file}/data/{i}/features-module.transformer.resblocks.20.csv', names=list(range(4)))#.drop_duplicates(0, keep='last')
+
+
+                ax.plot(df.iloc[:, 0], df.iloc[:, 2], color=color)
+                #ax.plot(df.iloc[:, 0], df.iloc[:, 1], color=color, alpha=0.3)
+                ax.plot(df.iloc[:, 0], df.iloc[:, 3], color=color, alpha=0.6)
+                ax.set_yscale('log')
+                ax.set_ylabel('Feature mean and max (block 10)')
+
+                
+
+        #print(f'/fsx-labs/mitchellw/experiments/blowup-examples/{file}/data/{i}/params-module.transformer.resblocks.30.attn.out_proj.weight.csv')
+        if log_level >= 3:
+            ax = axlist[2]
+            for i in range(1):
+                #layer = 'params-module.logit_scale.csv'
+                #layer = 'params-module.positional_embedding.csv' # YES!
+                #layer = 'params-module.text_projection.csv' # NO!
+                layer = 'params-module.token_embedding.weight.csv'
+                df = pd.read_csv(f'/fsx-labs/mitchellw/experiments/blowup-examples/{file}/data/{i}/{layer}', names=list(range(13)))
+                df = proc(df, lim)
+                ax.plot(df.iloc[:, 0], df.iloc[:, 4], color=color)
+                #ax.plot(df.iloc[:, 0], df.iloc[:, 5], color=color, alpha=0.6)
+                ax.plot(df.iloc[:, 0], df.iloc[:, 6], color=color, alpha=0.3)
+                ax.set_yscale('log')
+                ax.set_ylabel('token_embedding grad mean and max')
+
+
+
+    for j, ax in enumerate(axlist):
+        if j == 0:
+            ax.legend()
+        ax.grid()
+        ax.set_xlabel('Iterations')
+        vv = 3787
+        dd = 1e2
+        # vv = 2186
+        # dd = 1e3
+        vv =8010
+        dd = 1e1
+        continue
+        ax.axvline(vv, linestyle='--', color='gray')
+        ax.set_xlim(vv-dd, vv+dd)
+
+    plt.savefig('scripts/examples/loss_plot.png', bbox_inches='tight')
+
+
+
+
+"""
+0 step,
+1 p.abs().mean().item(), #'weight_means'
+2 p.abs().std().item(), # 'weight_std'
+3 p.abs().max().item(), # 'weight_max'
+4 p.grad.abs().mean().item(), #'grad_means'
+5 p.grad.abs().std().item(), # 'grad_std'
+6 p.grad.abs().max().item(), # 'grad_max'
+optimizer.state[p]['exp_avg'].abs().mean().item(), #'v_means'
+optimizer.state[p]['exp_avg'].abs().std().item(), # 'v_std'
+optimizer.state[p]['exp_avg'].abs().max().item(), # 'v_max'
+optimizer.state[p]['exp_avg_sq'].abs().mean().item(), #'g_means'
+optimizer.state[p]['exp_avg_sq'].abs().std().item(), # 'g_std'
+optimizer.state[p]['exp_avg_sq'].abs().max().item(), # 'g_max'
+"""
+
+"""
+        features = x.abs()
+        to_log = [
+            _iter,
+            features.std().item(), # std
+            features.mean().item(), # mean
+            features.max().item(), # max
+        ]
+"""
